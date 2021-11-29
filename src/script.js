5a494968c161015ec1e064f2e03fc8233f262a26
Florian <florian@bearstudio.fr>
2021-11-29 14:19:50 +0100

    Setup project

---
+++ src/script.js
@@ -0,0 +1,48 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+const mesh2 = new THREE.Mesh(geometry, material2)
+mesh.position.x = -2
+mesh.position.y = -2
+mesh.position.z = -3
+mesh2.position.x = 0.9
+mesh2.position.y = -0.7
+mesh2.position.z = 0.6
+scene.add(mesh, mesh2)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
